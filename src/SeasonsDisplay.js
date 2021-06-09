778bfc138050f7ccead7313d38d65ee897332092
robbender <robb.webdev@gmail.com>
2021-06-09 10:29:25 -0700

    first commit

---
+++ src/SeasonsDisplay.js
@@ -0,0 +1,35 @@
+import './SeasonDisplay.css';
+import React from "react";
+
+const seasonConfig = {
+    summer: {
+        message: 'Let\'s hit the beach!',
+        iconName: 'sun'
+    },
+    winter: {
+        message: 'Burr, it\'s cold!',
+        iconName: 'snowflake'
+    }
+};
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9 ) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat >0 ? 'winter' : 'summer';
+    }
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const { message, iconName } = seasonConfig[season];
+    return(
+        <div className={`season-display ${season}`}>
+        <i className={`icon-left massive ${iconName} icon`} />
+        <h1>{ message }</h1>
+        <i className={`icon-right massive ${iconName} icon`} />
+        </div>
+    );
+};
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from './SeasonsDisplay';
+import "semantic-ui-css/semantic.min.css";
+import Spinner from './Spinner';
+class App extends React.Component {
+  state = { lat: null, errorMessage: '' };
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      // success callback 1
+      (postition) => this.setState({ lat: postition.coords.latitude }),
+      // failure callback 2
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={ this.state.lat } />;
+    }
+    return (
+      <div className>
+        <h3>Working...</h3>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
