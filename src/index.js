75a2820d2e3f11a1c1212adbdb2bc5bf347d726c
robbender <robb.webdev@gmail.com>
2021-06-09 16:01:20 -0700

    Combined conditions and added navigator check

--- src/index.js
+++ src/index.js
@@ -1,33 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from './SeasonsDisplay';
+import SeasonDisplay from "./SeasonsDisplay";
 import "semantic-ui-css/semantic.min.css";
-import Spinner from './Spinner';
-import './SeasonDisplay.css';
+import Spinner from "./Spinner";
+import "./SeasonDisplay.css";
 class App extends React.Component {
-  state = { lat: null, errorMessage: '' };
+  state = { lat: null, errorMessage: "" };
 
   componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      // success callback 1
-      (postition) => this.setState({ lat: postition.coords.latitude }),
-      // failure callback 2
-      (err) => this.setState({ errorMessage: err.message })
-    );
+    if ("geolocation" in navigator) {
+      console.log("%cGeolocation available.", "color:green");
+      window.navigator.geolocation.getCurrentPosition(
+        // success callback 1
+        // (position) => console.log(position),
+        (position) =>
+          this.setState({
+            lat: position.coords.latitude,
+            city: position.coords.location,
+          }),
+        // failure callback 2
+        (err) => this.setState({ errorMessage: err.message })
+      );
+    } else {
+      console.log("%cGeolocation not available.", "color:red");
+    }
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return (
+        <div className="error-message">
+          Error: {this.state.errorMessage}
+          <br />
+          <br />
+          Please set default location or allow browser to detect your location.
+        </div>
+      );
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return (
+        <div>
+          {/* <h1>{this.state.city}</h1> */}
+          <SeasonDisplay lat={this.state.lat} />
+        </div>
+      );
+    }
+    return <Spinner message="To continue please accept location request." />;
   }
 
   render() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div className="error-message">Error: {this.state.errorMessage}
-      <br/> 
-      <br/>Please set default location or allow browser to detect your location.</div>;
-    }
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={ this.state.lat } />;
-    }
-    return (
-      <Spinner message="To continue please accept location request."/>
-    );
+    return <div className="">{this.renderContent()}</div>;
   }
 }
 
